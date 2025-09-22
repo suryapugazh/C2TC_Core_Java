038641bc196c32c38b8d1002815eb8ae45fc9bd4
Surya <suryapugazh27@gmail.com>
2025-09-22 15:23:41 +0530

    First Core Java Program

---
+++ CJ_Programs/src/Day1/FirstProgram.java
@@ -0,0 +1,10 @@
+package Day1;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		System.out.print("Welcome to Java.....");
+		
+	}
+
+}
---
+++ CJ_Programs/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module CJ_Programs {
+}
