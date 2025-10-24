4d05ec6ad247cf6905d6140367d9b57e39c9d586
Surya <suryapugazh27@gmail.com>
2025-10-24 22:41:49 +0530

    Day 11 Completed

---
+++ CJ_Programs/src/com/tnsif/day11/v1/Demo.java
@@ -0,0 +1,8 @@
+package com.tnsif.day11.v1;
+public class Demo {
+	public static void main(String[] args) {
+		System.out.println("the program continues....");
+		@SuppressWarnings("unused")
+		int data=100/0;
+	}
+}
